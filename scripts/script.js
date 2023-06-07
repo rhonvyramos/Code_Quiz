ef9e30e0266ce58398e33d5a02a3b2b551c8944a
rhonvyramos <rhonvyramos@gmail.com>
2023-06-07 11:58:09 -0400

    added basic functionality to inhibit more questions to display when maximum number of questions have been displayed

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,7 @@ let content_highscore = document.getElementById("content_highscore");
 let content_scoreboard = document.getElementById("content_scoreboard");
 
 let button_quiz_start = document.getElementById("button_quiz_start");
+let button_next_q = document.getElementById("go_next");
 
 let q_datatypes = {
     "question":"Commonly used data types DO NOT include: ",
@@ -25,15 +26,42 @@ let q_ifelse = {
     ]
 }
 
-let list_questions = [q_datatypes, q_ifelse];
+let q_jsarrays = {
+    "question":"Arrays in JavaScript can be used to store _____.",
+    "answers": [
+        "all of the above",
+        "numbers and strings",
+        "other arrays",
+        "booleans"
+    ]
+}
+
+let list_questions = [q_datatypes, q_ifelse, q_jsarrays];
+let numQuestions = list_questions.length;
+var x = numQuestions;
 
 button_quiz_start.addEventListener("click", function() {
     console.log("quiz started");
 
-    let randQuestion = Math.floor(Math.random() * list_questions.length);
+    let randQuestion = Math.floor(Math.random() * numQuestions);
 
     document.getElementById("quiz_question").textContent = list_questions[randQuestion]["question"];
     content_intro.style.display = "none";
     content_quiz.style.display = "flex";
 });
+
+go_next.addEventListener("click", function() {
+
+    let randQuestion = Math.floor(Math.random() * numQuestions);
+
+    if (x > 0) {
+        document.getElementById("quiz_question").textContent = list_questions[randQuestion]["question"];
+        x -= 1;
+        console.log(x)
+    } else {
+        return;
+    }
+
+
+})
 
